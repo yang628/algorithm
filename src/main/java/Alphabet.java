0bf09e1ad1ad1339e401ae7b32e2a43a1c5e5768
cheqianzi <cheqianzi@2dfire.com>
2018-01-01 20:43:39 +0800

    p453 字母表

---
+++ src/main/java/Alphabet.java
@@ -0,0 +1,273 @@
+import edu.princeton.cs.algs4.StdOut;
+
+/******************************************************************************
+ *  Compilation:  javac Alphabet.java
+ *  Execution:    java Alphabet
+ *  Dependencies: StdOut.java
+ *  
+ *  A data type for alphabets, for use with string-processing code
+ *  that must convert between an alphabet of size R and the integers
+ *  0 through R-1.
+ *
+ *  Warning: supports only the basic multilingual plane (BMP), i.e,
+ *           Unicode characters between U+0000 and U+FFFF.
+ *
+ ******************************************************************************/
+
+public class Alphabet {
+
+    /**
+     *  The binary alphabet { 0, 1 }.
+     */
+    public static final Alphabet BINARY = new Alphabet("01");
+
+    /**
+     *  The octal alphabet { 0, 1, 2, 3, 4, 5, 6, 7 }.
+     */
+    public static final Alphabet OCTAL = new Alphabet("01234567");
+
+    /**
+     *  The decimal alphabet { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }.
+     */
+    public static final Alphabet DECIMAL = new Alphabet("0123456789");
+
+    /**
+     *  The hexadecimal alphabet { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F }.
+     */
+    public static final Alphabet HEXADECIMAL = new Alphabet("0123456789ABCDEF");
+
+    /**
+     *  The DNA alphabet { A, C, T, G }.
+     */
+    public static final Alphabet DNA = new Alphabet("ACGT");
+
+    /**
+     *  The lowercase alphabet { a, b, c, ..., z }.
+     */
+    public static final Alphabet LOWERCASE = new Alphabet("abcdefghijklmnopqrstuvwxyz");
+
+    /**
+     *  The uppercase alphabet { A, B, C, ..., Z }.
+     */
+
+    public static final Alphabet UPPERCASE = new Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    /**
+     *  The protein alphabet { A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, V, W, Y }.
+     */
+    public static final Alphabet PROTEIN = new Alphabet("ACDEFGHIKLMNPQRSTVWY");
+
+    /**
+     *  The base-64 alphabet (64 characters).
+     */
+    public static final Alphabet BASE64 = new Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+
+    /**
+     *  The ASCII alphabet (0-127).
+     */
+    public static final Alphabet ASCII = new Alphabet(128);
+
+    /**
+     *  The extended ASCII alphabet (0-255).
+     */
+    public static final Alphabet EXTENDED_ASCII = new Alphabet(256);
+
+    /**
+     *  The Unicode 16 alphabet (0-65,535).
+     */
+    public static final Alphabet UNICODE16      = new Alphabet(65536);
+
+
+    private char[] alphabet;     // 字母表中的字符
+    private int[] inverse;       // 指数
+    private final int R;         // 字母表的基数
+
+    /**
+     * Initializes a new alphabet from the given set of characters.
+     *
+     * @param alpha the set of characters
+     */
+    public Alphabet(String alpha) {
+
+        // check that alphabet contains no duplicate chars 主要检查改定义的字表中，是否有重复的。有则报错，默认全都是false。
+        boolean[] unicode = new boolean[Character.MAX_VALUE];
+
+        for (int i = 0; i < alpha.length(); i++) {
+            char c = alpha.charAt(i);
+            if (unicode[c]){
+                throw new IllegalArgumentException("Illegal alphabet: repeated character = '" + c + "'");
+            }
+            //在对应的字符 C（对应的字符的十进制）位置 传入true。  字符的ASIIC码对应数组的索引
+            unicode[c] = true;
+        }
+        //字符传转成字符数组
+        alphabet = alpha.toCharArray();
+        R = alpha.length();
+        inverse = new int[Character.MAX_VALUE];
+        for (int i = 0; i < inverse.length; i++){
+            inverse[i] = -1;
+        }
+        // 由于R可以大到65,536，所以不能使用char
+        for (int c = 0; c < R; c++){
+            //每一个字符数组 所对应的元素的[ASSIC码]做为inverse的索引。
+            inverse[alphabet[c]] = c;
+        }
+
+    }
+
+    /**
+     * Initializes a new alphabet using characters 0 through R-1.
+     *
+     * @param radix the number of characters in the alphabet (the radix R)
+     */
+    private Alphabet(int radix) {
+        this.R = radix;
+        alphabet = new char[R];
+        inverse = new int[R];
+
+        // can't use char since R can be as big as 65,536
+        for (int i = 0; i < R; i++) {
+            alphabet[i] = (char) i;
+        }
+        for (int i = 0; i < R; i++){
+            inverse[i] = i;
+        }
+
+    }
+
+    /**
+     * Initializes a new alphabet using characters 0 through 255.
+     */
+    public Alphabet() {
+        this(256);
+    }
+
+    /**
+     * Returns true if the argument is a character in this alphabet.
+     *
+     * @param  c the character
+     * @return {@code true} if {@code c} is a character in this alphabet;
+     *         {@code false} otherwise
+     */
+    public boolean contains(char c) {
+        return inverse[c] != -1;
+    }
+
+    /**
+     * Returns the number of characters in this alphabet (the radix).
+     * 
+     * @return the number of characters in this alphabet
+     * @deprecated Replaced by {@link #radix()}.
+     */
+    @Deprecated
+    public int R() {
+        return R;
+    }
+
+    /**
+     * Returns the number of characters in this alphabet (the radix).
+     * 
+     * @return the number of characters in this alphabet
+     */
+    public int radix() {
+        return R;
+    }
+
+    /**
+     * Returns the binary logarithm of the number of characters in this alphabet.
+     * 
+     * @return the binary logarithm (rounded up) of the number of characters in this alphabet
+     */
+    public int lgR() {
+        int lgR = 0;
+        for (int t = R-1; t >= 1; t /= 2) {
+            lgR++;
+        }
+        return lgR;
+    }
+
+    /**
+     * Returns the index corresponding to the argument character.
+     * 
+     * @param  c the character
+     * @return the index corresponding to the character {@code c}
+     * @throws IllegalArgumentException unless {@code c} is a character in this alphabet
+     */
+    public int toIndex(char c) {
+        if (c >= inverse.length || inverse[c] == -1) {
+            throw new IllegalArgumentException("Character " + c + " not in alphabet");
+        }
+        //返回在 字符c(对应的十进制整数)
+        return inverse[c];
+    }
+
+    /**
+     * Returns the indices corresponding to the argument characters.
+     * 
+     * @param  s the characters
+     * @return the indices corresponding to the characters {@code s}
+     * @throws IllegalArgumentException unless every character in {@code s}
+     *         is a character in this alphabet
+     */
+    public int[] toIndices(String s) {
+        //1.把字符串转化为字符数组
+        char[] source = s.toCharArray();
+        //2.创建一个对应的长度的数组
+        int[] target  = new int[s.length()];
+
+        for (int i = 0; i < source.length; i++){
+            target[i] = toIndex(source[i]);
+        }
+        return target;
+    }
+
+    /**
+     * Returns the character corresponding to the argument index.
+     * 
+     * @param  index the index
+     * @return the character corresponding to the index {@code index}
+     * @throws IllegalArgumentException unless {@code 0 <= index < R}
+     */
+    public char toChar(int index) {
+        if (index < 0 || index >= R) {
+            throw new IllegalArgumentException("index must be between 0 and " + R + ": " + index);
+        }
+        return alphabet[index];
+    }
+
+    /**
+     * Returns the characters corresponding to the argument indices.
+     * 
+     * @param  indices the indices
+     * @return the characters corresponding to the indices {@code indices}
+     * @throws IllegalArgumentException unless {@code 0 < indices[i] < R}
+     *         for every {@code i}
+     */
+    public String toChars(int[] indices) {
+        StringBuilder s = new StringBuilder(indices.length);
+        for (int i = 0; i < indices.length; i++) {
+            s.append(toChar(indices[i]));
+        }
+        return s.toString();
+    }
+
+    /**
+     * Unit tests the {@code Alphabet} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int[]  encoded1 = Alphabet.BASE64.toIndices("NowIsTheTimeForAllGoodMen");
+        StdOut.println(encoded1);
+        String decoded1 = Alphabet.BASE64.toChars(encoded1);
+        StdOut.println(decoded1);
+
+        int[]  encoded2 = Alphabet.DNA.toIndices("AACGAACGGTTTACCCCG");
+        String decoded2 = Alphabet.DNA.toChars(encoded2);
+        StdOut.println(decoded2);
+
+        int[]  encoded3 = Alphabet.DECIMAL.toIndices("01234567890123456789");
+        String decoded3 = Alphabet.DECIMAL.toChars(encoded3);
+        StdOut.println(decoded3);
+    }
+}
